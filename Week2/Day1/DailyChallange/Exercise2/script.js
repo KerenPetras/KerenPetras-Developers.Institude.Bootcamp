e53dab46b3a4879bf9bf0d8a02b12608a1fa4155
KerenPetras <jackqlinep@gmail.com>
2023-01-22 15:16:50 +0200

    Day1 DailyChallange Update 1/22/2023

---
+++ Week2/Day1/DailyChallange/Exercise2/script.js
@@ -0,0 +1,14 @@
+// Exercise 2:
+// Using this array :
+
+// const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// Access and then console.log “Oranges”.
+// Bonus: If you would like more array exercises take a look at the link below.
+
+// Array Exercises
+
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log(moreFruits);
+console.log(moreFruits[1]);
+console.log(moreFruits[1][1]); //oranges in index number 1
+console.log(moreFruits[1][1][0]); //becauses the oranges were still in array so, and 0 was the only index in the orange array;
