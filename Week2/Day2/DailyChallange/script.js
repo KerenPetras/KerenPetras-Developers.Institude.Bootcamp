49f0ec0195ca10de10d81592b995aee97f7b7a04
KerenPetras <jackqlinep@gmail.com>
2023-01-23 17:18:50 +0200

    DailyChallange Day2 updated

--- Week2/Day2/DailyChallange/script.js
+++ Week2/Day2/DailyChallange/script.js
@@ -1,46 +1,40 @@
-// 🌟 Exercise 4: Group Chat
 // Instructions
-// Below is an array of users that are online in a group chat.
+// Create a variable called sentence. The value of the variable should be a string that contains the words “not” and “bad”.
+// For example, “The movie is not that bad, I like it”.
 
-// const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
-// Using the array above, console.log the number of users that are connected to the group chat based on the following rules:
-// If there is no users (the users array is empty), console.log “no one is online”.
-// If there is 1 user, console.log “<name_user> is online”.
-// If there are 2 users, console.log “<name_user1> and <name_user2> are online”.
-// If there are more than 2 users, console.log the first two names in the users array and the number of additional users online.
-// For example, if there are 5 users, it should display:
-// name_user1, name_user2 and 3 more are online
+// Create a variable called wordNot where it’s value is the first appearance (ie. the position) of the substring “not” (from the sentence variable).
 
-const users = ["KerenPet","Pikachu", "Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+// Create a variable called wordBad where it’s value is the first appearance (ie. the position) of the substring “bad” (from the sentence variable).
 
-//PART 1
-let userCount = users.length;
-let remaingUsers = (users.length)-2;
-console.log(userCount);
+// If the word “bad” comes after the word “not”, you should replace the whole “not…bad” substring with “good”, then console.log the result.
+// For example, the result here should be : “The movie is good, I like it”
+// If the word “bad” does not come after “not” or the words are not in the sentence, console.log the original sentence.
+// Example:
 
-//PART 2
-if (userCount === 0) {
-    console.log("No one is Online")
-} else {console.log("Someone is Online")};
-console.log(users);
+//   Your string is : 'This dinner is not that bad ! You cook well', 
+//   --> the result is : 'This dinner is good ! You cook well'
 
-//PART 3
-if (userCount === 1) {
-    console.log(` ${users[0]} is Online`);
-} else {console.log("Someone is Online")};
-console.log(users);
+//   Your string is : 'This movie is not so bad !' 
+//   --> the result is : 'This movie is good !'
+
+//   Your string is : 'This dinner is bad !' 
+//   --> the result is : 'This dinner is bad !'
+
+//PART1
+let sentence = "The movie is not that bad I like it";
+
+//PART2
+let wordNot = sentence.split('not');
+console.log(wordNot);
+//PART3
+let wordBad = sentence.split('bad');
+console.log(wordBad);
 
 //PART 4
-if (userCount === 2) {
-    console.log(` ${users[0]} and ${users[1]} is Online`);
-} else {console.log("There's more than 2 users Online")};
-console.log(users);
+let word = sentence.split(' ');
+console.log(word[3]);// wordNot // index 3 is smaller then idex 5
+console.log(word[5]); // wordBad // index 5 is bigger then index 3 
 
-//PART 5
-if (userCount > 2) {
-    console.log(` ${users[0]} and ${users[1]} and ${remaingUsers} more are Online`);
-} else {console.log("Theres more than 2 users Online")};
-console.log(users);
-
-
-
+if(wordBad>wordNot){
+console.log("The movie is good I like it")
+}else  {console.log(sentence)};
