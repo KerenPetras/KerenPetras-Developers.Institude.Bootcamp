521daadc28fd3afe914312af18cd55fca42e67f9
KerenPetras <jackqlinep@gmail.com>
2023-02-19 02:17:45 +0200

    daily challange w4 d2

--- Week3/Day2/DailyChallange/script.js
+++ Week3/Day2/DailyChallange/script.js
@@ -38,3 +38,70 @@
 
 //  </body>
 // </html>
+
+// Get the form element and the story span element
+const form = document.querySelector('#libform');
+const storySpan = document.querySelector('#story');
+
+// Get the input elements
+const nounInput = document.querySelector('#noun');
+const adjectiveInput = document.querySelector('#adjective');
+const personInput = document.querySelector('#person');
+const verbInput = document.querySelector('#verb');
+const placeInput = document.querySelector('#place');
+
+// Function to generate the story
+function generateStory() {
+  // Get the values from the inputs
+  const nounValue = nounInput.value;
+  const adjectiveValue = adjectiveInput.value;
+  const personValue = personInput.value;
+  const verbValue = verbInput.value;
+  const placeValue = placeInput.value;
+
+  // Check that all values are filled in
+  if (nounValue && adjectiveValue && personValue && verbValue && placeValue) {
+    // Generate the story using the input values
+    const story = `${personValue} went to the ${adjectiveValue} ${nounValue} and ${verbValue} there. Afterwards, they went to ${placeValue}.`;
+
+    // Display the story in the span element
+    storySpan.textContent = story;
+  } else {
+    // If any values are missing, display an error message in the console
+    console.error('Please fill in all values');
+  }
+}
+
+// Call the generateStory function when the form is submitted
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // prevent the form from submitting
+  generateStory();
+});
+
+// Bonus: add a shuffle button
+const shuffleButton = document.createElement('button');
+shuffleButton.textContent = 'Shuffle';
+storySpan.after(shuffleButton);
+
+shuffleButton.addEventListener('click', function() {
+  const stories = [
+    `${personInput.value} went to the ${adjectiveInput.value} ${nounInput.value} and ${verbInput.value} there. Afterwards, they went to ${placeInput.value}.`,
+    `Once upon a time, there was a ${adjectiveInput.value} ${nounInput.value} named ${personInput.value}. They loved to ${verbInput.value} in ${placeInput.value}.`,
+    `${personInput.value} and their friends went on a trip to ${placeInput.value}. They had to bring a lot of ${adjectiveInput.value} ${nounInput.value} for the journey. When they got there, they decided to ${verbInput.value} and have fun.`,
+  ];
+
+  // Remove the current story from the array of possible stories
+  const currentStory = storySpan.textContent;
+  const indexToRemove = stories.indexOf(currentStory);
+  if (indexToRemove !== -1) {
+    stories.splice(indexToRemove, 1);
+  }
+
+  // Pick a random story from the remaining stories
+  const randomIndex = Math.floor(Math.random() * stories.length);
+  const randomStory = stories[randomIndex];
+
+  // Display the new story
+  storySpan.textContent = randomStory;
+});
+
