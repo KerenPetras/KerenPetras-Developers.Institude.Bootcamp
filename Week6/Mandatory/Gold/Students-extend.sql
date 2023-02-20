fca6a448aac06be2df422519a0294afe4cb0f17e
KerenPetras <jackqlinep@gmail.com>
2023-02-20 02:20:01 +0200

    exercise gold w6 d1

---
+++ Week6/Mandatory/Gold/Students-extend.sql
@@ -0,0 +1,11 @@
+-- SELECT * FROM students;
+
+--1
+-- SELECT first_name, last_name,birth_date FROM students ORDER BY last_name LIMIT 4;
+
+--2
+-- SELECT first_name, last_name,birth_date FROM students ORDER BY birth_date DESC;
+
+--3
+-- SELECT first_name, last_name, birth_date FROM students WHERE id_student IN (3,4,5,6);
+
