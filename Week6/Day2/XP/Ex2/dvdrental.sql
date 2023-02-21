ce3de65dac7aac0b5f182ae1c4c719d8498e3fdc
KerenPetras <jackqlinep@gmail.com>
2023-02-21 13:42:33 +0200

    w6 d2 dvdrental

--- Week6/Day2/XP/Ex2/dvdrental.sql
+++ Week6/Day2/XP/Ex2/dvdrental.sql
@@ -1,4 +1,5 @@
 -- We will use the newly installed dvdrental database.
+--- DVD RENTAL RESTORE; WEEK 6 DAY2
 
 -- In the dvdrental database write a query to select all the columns from the “customer” table.
 SELECT * FROM customer;
@@ -7,16 +8,23 @@ SELECT * FROM customer;
 SELECT concat(first_name, ' ' ,last_name) AS full_name FROM customer;
 
 -- Lets get all the dates that accounts were created. Write a query to select all the create_date from the “customer” table (there should be no duplicates).
+SELECT create_date FROM customer;
+SELECT create_date,COUNT(*) AS CNT FROM customer GROUP BY create_date HAVING COUNT(*) > 1;
 
 -- Write a query to get all the customer details from the customer table, it should be displayed in descending order by their first name.
+SELECT * FROM customer ORDER BY first_name DESC;
 
 -- Write a query to get the film ID, title, description, year of release and rental rate in ascending order according to their rental rate.
+SELECT film_id,title,description,release_year FROM film; 
 
 -- Write a query to get the address, and the phone number of all customers living in the Texas district, these details can be found in the “address” table.
+SELECT address,phone,district FROM address WHERE district ILIKE 'Texas';
 
 -- Write a query to retrieve all movie details where the movie id is either 15 or 150.
+SELECT * FROM film WHERE (film_id >='15') AND NOT(film_id >='151');
 
 -- Write a query which should check if your favorite movie exists in the database. Have your query get the film ID, title, description, length and the rental rate, these details can be found in the “film” table.
+SELECT film_id,title,description,length,rental_rate FROM film;
 
 -- No luck finding your movie? Maybe you made a mistake spelling the name. Write a query to get the film ID, title, description, length and the rental rate of all the movies starting with the two first letters of your favorite movie.
 
