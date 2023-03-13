9bf3e48b88fec0b072aab8fecd56c3dd20fd64c2
KerenPetras <jackqlinep@gmail.com>
2023-03-14 00:44:28 +0200

    review 3/14

--- Week2/Day3/ClassExercise/Exercise1/script.js
+++ Week2/Day3/ClassExercise/Exercise1/script.js
@@ -2,22 +2,21 @@
 // 1. Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
 // Sample Output: //"0 is even" //"1 is odd" //"2 is even"
 
-// const newNumber = [];
-// for (let i = 0; i < 16; i++) {
-// if (i%2===0) {
-//     console.log(i+ " is even ");
+const newNumber = [];
+for (let i = 1; i < 16; i++) {
+if (i%2===0) {
+    console.log(i+ " is even ");
 
-// }else {console.log(i+ " is odd")}
+}else {console.log(i+ " is odd")}
+}
 
-// }
-
-const prices = [23, 15, 34, 10];
+// const prices = [23, 15, 34, 10];
 // Create a variable sum equals to 0
 // Loop over this array, to get the sum of all the numbers
 // Where should you console log the sum ?
 
-for (let i = 0; i < prices.length ; i++) {
-sum = sum+prices[i]
-}
+// for (let i = 0; i < prices.length ; i++) {
+// sum = sum+prices[i]
+// }
 
-console.log(sum);
+// console.log(sum);
---
+++ Week2/Day3/GoldXP/script.js
@@ -0,0 +1,54 @@
+// Exercise 1 : Divisible By Three
+// Instructions
+// let numbers = [123, 8409, 100053, 333333333, 7]
+// Loop through the array above and determine whether or not each number is divisible by three.
+// Each time you loop console.log true or false.
+
+// let numbers = [123, 8409, 100053, 333333333, 7];
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i]%3===0) {
+//         console.log( numbers[i] + " the number is devided by 3");
+// }else {console.log(numbers[i] + " the number is NOT devided by 3")};
+// };
+
+// Exercise 2 : Attendance
+// Instructions
+// const guestList = {
+//     randy: "Germany",
+//     karla: "France",
+//     wendy: "Japan",
+//     norman: "England",
+//     sam: "Argentina"
+//   };
+  
+//   const student = prompt("Please Enter Your Name: ");
+  
+//   let isFound = false;
+  
+//   for (let name in guestList) {
+//     if (name === student) {
+//       console.log(`Hey I'm ${student} and I'm from ${guestList[name]}`);
+//       isFound = true;
+//       break;
+//     }
+//   }
+  
+//   if (!isFound) {
+//     console.log(`The ${student} is not found`);
+//   }
+  
+// Given the object above where the key is the student’s name and the value is the country they are from.
+// Prompt the student for their name.
+// If the name is in the object, console.log the name of the student and the country they come from.
+// For example: "Hi! I'm [name], and I'm from [country]."
+// Hint: You don’t need to use a for loop, just look up the statement if ... in
+
+// If the name is not in the object, console.log: "Hi! I'm a guest."
+
+// Exercise 3 : Playing With Numbers
+// Instructions
+let age = [20,5,12,43,98,55];
+// Requirements : Don’t use built-in Javascript methods to answer the following questions. You have to create the logic by yourself. Use simple for loops.
+
+// 1. Console.log the sum of all the numbers in the age array.
+// 2. Console.log the highest age in the array.
